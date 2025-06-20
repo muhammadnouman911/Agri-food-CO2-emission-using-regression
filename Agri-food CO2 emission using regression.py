b2ef498a756568d1b38558a5c3d9ea561e83a175
Muhammad Nouman <muhammadnoumanf22@gmail.com>
2025-06-20 21:05:02 +0500

    Add files via upload

---
+++ Agri-food CO2 emission using regression.py
@@ -0,0 +1,12 @@
+
+import pandas as pd
+import numpy as np
+
+
+
+
+# Load the data
+dataset = pd.read_csv('C:/Users/PMYLS/Desktop/Agrofood_co2_emission.csv')
+
+# Check the first few rows
+print(dataset.head())
